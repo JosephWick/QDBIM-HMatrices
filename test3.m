dcd428878e773b814ac2b6c7e2809a0e617095ff
Joseph Wick <josephwick@outlook.com>
2021-07-19 20:12:09 -0700

    Update test3.m

--- test3.m
+++ test3.m
@@ -88,16 +88,93 @@ end
 
 % write_mesh_kvf
 function cm = write_mesh_kvf(o, p)
+  % get a filename
+  c.mesh_write_filename = make_base_fn(o);
+
+  c.do_uniform = o.do_uniform;
+
+  c.min_len = 0;
+  if (isinf(o.max_len))
+    c.max_len = min(diff(p.x([1 end])), diff(p.y([1 end])))/8;
+  else
+    c.max_len = o.max_len;
+  end
+
+  % make a tensor mesh, on which the resolution function f is set
+  % f has same units as x and y
+  c.x = p.x;
+  c.y = p.y;
+  c.f = p.h_star/(o.rfac*5);
+  c.command='mesh'
+  c.kvf = [make_base_fn(o) '_m'];
+
+  dc3dm.WriteKvf(c.kvf, c, true);
 
 end
 
 % write_build_kvf
 function cb = write_build_kvf(o)
 
+  bfn = make_base_fn)o);
+  c.mesh_read_filename = bfn;
+  c.build_write_filename = sprintf('%s_p%d', bfn, o.problem);
+
+  % depth_min == 0 means there is a free surface at the zero-depth boundary
+  c.depth_min=100;
+  if (o.want_free_surface) c.depth_min = 0; end
+  switch (o.problem)
+    case 1  % subduction, also the only option rn
+      % pos means that north boundary is at surface and south boundary has
+      % the velocity coundition below
+      c.dipdeg = 0;
+      % fault is periodic along-strike
+      c.ewpbc = 0;
+      % this is velocity boundary condition at depth
+      c.svbc = 0
+    otherwise
+      error(sprintf('%d is not a valid problem number.', o.problem));
+  end
+
+  c.neightborhood = o.neighborhood
+  c.bc_periodic_nlayers = 3;
+
+  c.command = 'build';
+  c.kvf = [bfn '_b'];
+
+  dcedm.WriteKvf(c.kvf, c, true);
+
 end
 
 % write_compress_kvf
 function cc = write_compress_kvf(o, r.cb)
+
+  switch (o.problem)
+    case 1
+      v = [1 2 0]
+      v = v/norm(v)
+      c.src_disl = v;
+      c.rcv_traction = c.src_disl;
+      c.component = 1
+    otherwise
+      error(sprintf('%d is not a valid problem number.', o.problem));
+    end
+
+    bfn = make_base_fn(o);
+    c.mesh_read_filename = bfn;
+    c.build_read_filename = cb.build_write_filename;
+    c.tol = p.tol;
+
+    c.hm_write_filename = sprintf('%s_to1$1.1f, cb.build_read_filename', ...
+     -log10(c.tol));
+
+    c.allow_overwrite = 1;
+    c.mu = 3e10;
+    c.nu = 0.25;
+    c.nthreads = o.nthreads;
+
+    c.command = 'compress';
+    c.kvf = [kvf '_c']
+    dcedm.WriteKvf(c.kvf, c, true);
 
 end
 
@@ -107,4 +184,11 @@ end
 % adds path to the dc3dm/matlab directory
 function addpaths()
   addpath /auto/home/jmwick/EarthquakeSimulation/dc3dm-main/matlab;
+end
+
+% make_base_fn()
+% generates a file name based on the inputted options
+function bfn = make_base_fn (o)
+  bfn = sprintf('%sdc3t_rf%1.2flf%1.2fvf%1.2fnbr%du%d', o.dir, o.rfac, ...
+                o.len_fac, o.vary_fac, o.neighborhood, o.do_uniform);
 end
