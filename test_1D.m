60c810986f48a30158db57331ba1ce0d2ceb243e
Joseph Wick <josephwick@outlook.com>
2021-07-27 10:43:30 -0700

    Update test_1D.m

--- test_1D.m
+++ test_1D.m
@@ -51,12 +51,12 @@ function y = mvp_test (r)
   n = hmmvp('getn', hm);
 
   % make a vector to multiply by
-  %a = zeros(12,12);
-  %a(4:8,4:8) = 1;
-  %x = reshape(a, [144, 1]);
+  a = zeros(100,100);
+  a(40:49,40:49) = 1;
+  x = reshape(a, [10000, 1]);
 
-  x = [0,0,0,0, 0,1,1,0, 0,1,1,0, 0,0,0,0];
-  x = x';
+  %x = [0,0,0,0, 0,1,1,0, 0,1,1,0, 0,0,0,0];
+  %x = x';
 
   y = hmmvp('mvp', hm, x);
 
