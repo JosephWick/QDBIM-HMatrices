af5d6e78e0d0182d856f9c0134bd1a39ec78350d
Joseph Wick <josephwick@outlook.com>
2021-07-27 16:54:54 -0700

    Create QDBIM2D_hm.m

---
+++ QDBIM2D_hm.m
@@ -0,0 +1,10 @@
+% QDBIM2D_hm
+% basically QDBIM2D but with hmmvp stress kernel
+
+function varargout = exmb (varargin)
+
+  [varargout{1:nargout}] = feval(varargin{:});
+
+end
+
+% --------------------- Public -----------------------------
