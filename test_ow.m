416901158c1eab304762b3185bd714698c89b2a9
Joseph Wick <josephwick@outlook.com>
2021-07-28 13:44:34 -0700

    Update test_ow.m

--- test_ow.m
+++ test_ow.m
@@ -17,7 +17,7 @@ n = 100;
 x = linspace(-0.5*n, 0.5*n, n);
 y = linspace(-0.5*n, 0.5*n, n);
 S_xy = zeros(n, n);
-
+U_xy = zeros(n,n);
 for i = 1:100
   for j = 1:100
     [sucess, u, grad_u] = DC3Dwrapper(alpha, [x(i), y(j), -obs_depth], ...
@@ -26,11 +26,12 @@ for i = 1:100
                                       [-0.7, 0.7], ...
                                       [1.0, 0.0, 0.0]);
 
-    S_xy(i,j) = ( grad(1,2) + grad(2,1) ) * mu;
+    S_xy(i,j) = ( grad_u(1,2) + grad_u(2,1) ) * mu;
+    %U_xy(i,j) = grad
 
   end
 end
 
 clf;
-imagesc(S_xy); title('okada_wrapper'); colorbar;
+imagesc(S_xy); title('okada wrapper'); colorbar;
 saveas(gcf, 'figures/test_ow.png');
