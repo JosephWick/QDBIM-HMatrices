fb3652606790b978c0abda7ed9394ed3b7239cc4
Joseph Wick <josephwick@outlook.com>
2021-08-19 15:30:32 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -54,6 +54,14 @@ function r = build()
   z = linspace(c.dz,lambdaZ,n);
   c.X = [x;y;z];
 
+  y2 = 15000;
+  ss.y2 = y2;
+  % tops of fault patches
+  ss.y3f = z - dz;
+  % width of fault patches
+  ss.Wf = ones(n,1)*dz;
+  ss.M = n;
+
   kvf('Write', c.kvf, c, 4);
   disp('run these in a shell:')
   cmd = ['    ../hmmvp-okada/bin/hmmvpbuild_omp ' c.kvf];
@@ -75,6 +83,23 @@ function r = build()
   z = linspace(c.dz,vW,m);
   [Y Z] = ndgrid(y,z);
   c.X = [x;Y(:)';Z(:)'];
+
+  % shear mesh
+  ss.Nx = n;
+  ss.Nz = m;
+  ss.polesz = transition+tan((0:ss.Nz)'*pi/(2.2*(ss.Nz+eps)))*transition;
+  % center of shear zone (x3)
+  ss.x3c=(ss.polesz(2:end)+ss.polesz(1:end-1))/2;
+  W=ss.polesz(2:end)-ss.polesz(1:end-1);
+
+  ss.polesxc=(2*y2/1e3:(2*y2)/(26e3):2*y2/1e3)'*1e3;
+  edges= floor(ss.Nx-length(ss.polesxc)+1)/2;
+  ss.polesxl=flipud(min(ss.polesxc)-tan((0:edges)'*pi/(2.2*(edges)+eps))*Transition);
+  ss.polesxr=max(ss.polesxc)+tan((0:edges)'*pi/(2.2*(edges)+eps))*Transition;
+  ss.polesx=[ss.polesxl(1:end-1);ss.polesxc;ss.polesxr(2:end)];
+
+  % center of shear zone (x2)
+  ss.x2c=(ss.polesx(2:end)+ss.polesx(1:end-1))/2;
 
   kvf('Write', c.kvf, c, 4);
   cmd = ['    ../hmmvp-okada/bin/hmmvpbuild_omp ' c.kvf];
@@ -174,6 +199,118 @@ function r = build()
   cmd = ['    ../hmmvp-okada/bin/hmmvpbuild_omp ' c.kvf];
   disp(cmd)
   r.sf1313 = c.write_hmat_filename;
+
+  r.ss = ss;
+
+end
+
+% runs numerical solution
+function solve(r)
+  addpaths();
+
+  % - frictional parameters -
+
+  % Confining pressure (MPa) and Temperature (K)
+  k = k  = 3.138; % thermal conductivity (W/m/K)
+  Cp = 1171 ; % specific heat (J/kg/K)
+  Rm = 3330 ; % mantle density (kg/m^3)
+
+  Pconf       = Rm*9.8*r.ss.x3c/1e6;  % Shear zones
+  Pconf_fault = Rm*9.8*(r.ss.y3f+dz); % Faults
+
+  Kappa     = k / (Rm * Cp); % Thermal diffusivity (m^2/s)
+  Age_plate = 2e15; % seconds
+  ss.Tprof  = 300+1380*erf(r.ss.x3c/(sqrt(4* Kappa * Age_plate)));  % Kelvin
+
+  % default friction properties (velocity-weakening friction)
+  % effective confining pressure (MPa)
+  ss.sigmab = 1000;
+
+  % frictional parameters
+  r.ss.a = 1e-3*ones(size(r.ss.y3f));
+  r.ss.b = r.ss.a+2.1e-4*ones(size(r.ss.y3f));
+
+  r.ss.mu0 = 0.2*ones(size(r.ss.y3f));
+  % characteristic weakening distance (m)
+  r.ss.L = 0.012*ones(size(r.ss.y3f));
+  % plate velocity (m/s)
+  r.ss.V_plate = 1e-9*ones(size(r.ss.y3f));
+  % reference slip rate (m/s)
+  r.ss.Vo = 1e-6*ones(size(r.ss.y3f));
+  % shear wave speed (m/s)
+  r.ss.Vs = 3e3*ones(size(r.ss.y3f));
+
+  % Velocity-strengthening at edges
+  top    = floor(5e3/(Transition/r.ss.M));
+  bottom = ceil(30e3/(Transition/r.ss.M));
+  r.ss.b(1:top)      = r.ss.a(1:top)-2.1e-4*ones(top,1);
+  r.ss.b(bottom:end) = r.ss.a(bottom:end)-2.1e-4*ones(length(r.ss.a(bottom:end)),1);
+
+  % - Rheology -
+  r.ss.e12p_plate = 0.0 %1e-14*ones(length(ss.x2c)*length(ss.x3c),1);
+  r.ss.e13p_plate = 0.0     %zeros(length(ss.x2c)*length(ss.x3c),1);
+  % Rheological Parameters
+  % Reference Strain Rate (for stress in MPa)
+  r.ss.Adif = 1e6*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  r.ss.Adis = 90 *ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  % Power-Law Exponent
+  r.ss.n = 3.5*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  % Activation Energy Wet Oliving (J/mol)
+  r.ss.Qdif = 335e3*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  r.ss.Qdis = 480e3*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  % Activation Volume (m^3/mol)
+  r.ss.Voldif = 4e-6*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  r.ss.Voldis = 11e-6*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  % Grain size (m)
+  r.ss.d    = 1e-2*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  r.ss.pexp = 3*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  % Water fugacity (H/10^6 Si)
+  r.ss.COH = 1000*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  r.ss.R   = 1.2*ones(length(r.ss.x3c)*length(r.ss.x2c),1);
+  % Pressure (Pa)
+  r.ss.P = repmat(1e6*Pconf',length(r.ss.x2c),1);
+  r.ss.P = reshape(r.ss.P,[length(r.ss.x2c)*length(r.ss.x3c),1]);
+  % Temperature (K)
+  Te0 = repmat(r.ss.Tprof',length(r.ss.x2c),1);
+  Te0 = reshape(Te0,[length(r.ss.x2c)*length(r.ss.x3c),1]);
+  % Coefficients for dislocation and diffusion creep
+  r.ss.Const_dis = r.ss.Adis.*exp(-(r.ss.Qdis+r.ss.P.*r.ss.Voldis)./(8.314.*Te0)).*r.ss.COH.^(r.ss.R);
+  r.ss.Const_diff =r.ss.Adif.*exp(-(r.ss.Qdif+r.ss.P.*r.ss.Voldif)./(8.314.*Te0)).*r.ss.COH.^(r.ss.R).*r.ss.d.^(-r.ss.pexp);
+  % Strengh profile
+  s120 = (r.ss.e12p_plate./r.ss.Const_dis).^(1./r.ss.n);
+  s130 = zeros(size(s120));
+  e120 = zeros(size(s120));
+  e130 = zeros(size(s120));
+  % Fault Strength
+  r.ss.strength = r.ss.sigmab.*(r.ss.mu0+(r.ss.a-r.ss.b).*log(r.ss.V_plate./r.ss.Vo))+G*r.ss.V_plate./(2*r.ss.Vs);
+
+  % - Numerical Solution -
+
+  % state parameters
+  r.ss.dgfF=3;
+  r.ss.dgfS=4;
+  %% Initialize State Vector
+  Y0=zeros(r.ss.M*r.ss.dgfF+length(r.ss.x2c)*length(r.ss.x3c)*r.ss.dgfS,1);
+
+  % Fault patches
+  Y0(1:ss.dgfF:ss.M*ss.dgfF)=zeros(size(ss.y3f));
+  Y0(2:ss.dgfF:ss.M*ss.dgfF)=ss.strength_W;
+  Y0(3:ss.dgfF:ss.M*ss.dgfF)=log(ss.Vo./ss.V_plate);
+
+  % Shear zones
+  Y0(ss.M*ss.dgfF+1:ss.dgfS:end)=s120;
+  Y0(ss.M*ss.dgfF+2:ss.dgfS:end)=s130;
+  Y0(ss.M*ss.dgfF+3:ss.dgfS:end)=e120;
+  Y0(ss.M*ss.dgfF+4:ss.dgfS:end)=e130;
+
+  % initialize the function handle with
+  % set constitutive parameters
+  yp=@(t,y) odeViscoelastic(t,y,ss);
+  tic
+  % Solve the system
+  options=odeset('Refine',1,'RelTol',3e-7,'InitialStep',1e-3,'MaxStep',3e6);
+  [t,Y]=ode45(yp,[0 1e10],Y0,options);
+  toc
 
 end
 
