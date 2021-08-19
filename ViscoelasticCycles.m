ad77a2e59ca33b38cb8865ebdb8d5226ba2d9e36
Joseph Wick <josephwick@outlook.com>
2021-08-19 15:48:59 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -246,7 +246,7 @@ function solve(r)
   top    = floor(5e3/(r.ss.lambdaZ/r.ss.M));
   bottom = ceil(30e3/(r.ss.lambdaZ/r.ss.M));
   r.ss.b(1:top)      = r.ss.a(1:top)-2.1e-4*ones(1,top);
-  r.ss.b(bottom:end) = r.ss.a(bottom:end)-2.1e-4*ones(length(1,r.ss.a(bottom:end)));
+  r.ss.b(bottom:end) = r.ss.a(bottom:end)-2.1e-4*ones(1,length(r.ss.a(bottom:end)));
 
   % - Rheology -
   r.ss.e12p_plate = 0.0 %1e-14*ones(length(ss.x2c)*length(ss.x3c),1);
