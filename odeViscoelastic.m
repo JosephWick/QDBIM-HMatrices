2f8834d83e7498404bc9ace663c8f7b24d706d68
Joseph Wick <josephwick@outlook.com>
2021-08-20 16:39:58 -0700

    Update odeViscoelastic.m

--- odeViscoelastic.m
+++ odeViscoelastic.m
@@ -85,6 +85,7 @@ disp('b')
 %t1 = hmmvp('mvp', hm.s12, (V-ss.V_plate));
 disp('b1')
 disp(hmmvp('getn', hm.fs1212))
+disp(size(e12p-ss.e12p_plate))
 t2 = hmmvp('mvp', hm.fs1212, (e12p-ss.e12p_plate), gM, lM);
 disp('b2')
 t3 = hmmvp('mvp', hm.fs1312, (e13p-ss.e13p_plate), gM, lM);
