4faa037905031db15d47915b0d53e491ef436d41
Joseph Wick <josephwick@outlook.com>
2021-08-20 16:05:25 -0700

    Update odeViscoelastic.m

--- odeViscoelastic.m
+++ odeViscoelastic.m
@@ -78,9 +78,18 @@ disp('a')
 Yp(1:ss.dgfF:ss.M*ss.dgfF)=V;
 disp('b')
 % Shear stress rate on fault due to fault and shear zones
-Yp(2:ss.dgfF:ss.M*ss.dgfF)=  hmmvp('mvp', hm.s12, (V-ss.V_plate)) + ...
-                             hmmvp('mvp', hm.fs1212, (e12p-ss.e12p_plate), gM, lM)+...
-                             hmmvp('mvp', hm.fs1312, (e13p-ss.e13p_plate), gM, lM);
+
+t1 = hmmvp('mvp', hm.s12, (V-ss.V_plate));
+disp('b1')
+t2 = hmmvp('mvp', hm.fs1212, (e12p-ss.e12p_plate), gM, lM);
+disp('b2')
+t3 = hmmvp('mvp', hm.fs1312, (e13p-ss.e13p_plate), gM, lM);
+disp('b3')
+
+%Yp(2:ss.dgfF:ss.M*ss.dgfF)=  hmmvp('mvp', hm.s12, (V-ss.V_plate)) + ...
+%                             hmmvp('mvp', hm.fs1212, (e12p-ss.e12p_plate), gM, lM)+...
+%                             hmmvp('mvp', hm.fs1312, (e13p-ss.e13p_plate), gM, lM);
+
 disp('c')
 % Rate of state
 Yp(3:ss.dgfF:ss.M*ss.dgfF)=(ss.Vo.*exp(-th)-V)./ss.L;
