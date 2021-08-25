06197c7aa21a3262ec4ab0e8e2e948a0243ad1d7
Joseph Wick <josephwick@outlook.com>
2021-08-25 13:52:12 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -371,7 +371,7 @@ function solve(r)
   ylabel('Depth (km)');
 
   f3=subplot(5,1,3);cla;
-  pcolor(t(1:end-1)/3.15e7,ss.x3c/1e3,log10(Ep)), shading flat
+  pcolor(t(1:end-1)/3.15e7,r.ss.x3c/1e3,log10(Ep)), shading flat
   set(gca,'YDir','reverse');
 
   caxis([log10(min(min(Ep))) log10(max(max(Ep)))]);
@@ -399,18 +399,18 @@ function solve(r)
   f1=figure(2);clf;set(gcf,'name','Time Step Evolution')
 
   subplot(5,1,1);cla;
-  pcolor(1:length(t)-1,ss.y3f/1e3,log10(V')), shading flat
+  pcolor(1:length(t)-1,r.ss.y3f/1e3,log10(V')), shading flat
   set(gca,'YDir','reverse');
 
   h=colorbar('Location','NorthOutside');
-  caxis([min(min(log10(V_W))) max(max(log10(V_W)))]);
+  caxis([min(min(log10(V))) max(max(log10(V)))]);
   colormap(f1,parula);
   title(h,'Slip Rate West (m/s)')
   xlabel('Time Steps')
   ylabel('Depth (km)');
 
   f3=subplot(5,1,3);cla;
-  pcolor(1:length(t)-1,ss.x3c/1e3,log10(Ep)), shading flat
+  pcolor(1:length(t)-1,r.ss.x3c/1e3,log10(Ep)), shading flat
   set(gca,'YDir','reverse');
 
   caxis([log10(min(min(Ep))) log10(max(max(Ep)))]);
