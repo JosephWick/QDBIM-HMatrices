c6e52a9924c07e51163f7c4967eeb1e24f266c26
Joseph Wick <josephwick@outlook.com>
2021-08-31 16:08:22 -0700

    ;

--- VCtiming.m
+++ VCtiming.m
@@ -21,7 +21,7 @@ function b = build()
   % grid edges
   ss.Ny = probL/shearYsize;
   ss.Nz = ss.Ny;
-  nc = (-ss.Nz/2:ss.Nz/2)
+  nc = (-ss.Nz/2:ss.Nz/2);
   shearZhat = transition+tan((0:ss.Nz)'*pi/(2.2*(ss.Nz+eps)))*transition;
   shearYhat = tan(nc*pi/(2.5*max(nc)))*32e3 / 1e3
   shearZhat(end)=[]; shearYhat(end)=[];
@@ -47,7 +47,7 @@ function b = build()
 
   c.transition = transition;
 
-  c.dz = size;
+  c.dz = shearYsize;
   c.tol = 1e-6;
   c.G = 30e3;
   c.command = 'compress';
