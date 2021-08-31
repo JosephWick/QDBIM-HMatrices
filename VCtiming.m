3daa24c2e021f6e8305c22f116086a0699a5f1f6
Joseph Wick <josephwick@outlook.com>
2021-08-31 13:55:58 -0700

    Update VCtiming.m

--- VCtiming.m
+++ VCtiming.m
@@ -51,7 +51,7 @@ function b = build()
   c.write_hmat_filename = './tmp/VCT';
   c.write_hd_filename = './tmp/VCT-hd';
   c.kvf = [c.write_hmat_filename '.kvf'];
-  kvf('Write', c.kvf, c, 32);
+  kvf('Write', c.kvf, c, 16);
 
   disp('run this in a shell: ')
   cmd = ['    ../hmmvp-okada/bin/hmmvpbuild_omp ' c.kvf];
