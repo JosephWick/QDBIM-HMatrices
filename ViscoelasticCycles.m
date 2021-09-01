6543958cca73b83090fb424fbd8dea30c284857b
Joseph Wick <josephwick@outlook.com>
2021-08-31 21:50:02 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -454,6 +454,7 @@ function out = solve(r)
   out.E = Ep;
 
   f3=subplot(5,1,3);cla;
+  disp(length(t))
   disp(size(r.ss.x3c(1:end-1)/1e3))
   disp(size(log10(Ep))))
   pcolor(1:length(t), r.ss.x3c(1:end-1)/1e3, log10(Ep)), shading flat
