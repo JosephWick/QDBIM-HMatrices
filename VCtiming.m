1b5459aa6f4d44572eea524772b0346ade039568
Joseph Wick <josephwick@outlook.com>
2021-09-01 10:42:25 -0700

    Update VCtiming.m

--- VCtiming.m
+++ VCtiming.m
@@ -16,7 +16,7 @@ function b = build()
   % 20, 40, 80, 160, 320
 
   transition = 40e3; %where shear zone starts
-  shearYsize = 5000;
+  shearYsize = 2500;
 
   % grid edges
   ss.Ny = probL/shearYsize;
