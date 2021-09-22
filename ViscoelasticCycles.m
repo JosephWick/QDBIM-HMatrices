7d0c76dd4014d070b11dd8b4ee3f9950d00696c5
Joseph Wick <josephwick@outlook.com>
2021-09-22 16:28:09 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -357,7 +357,7 @@ function out = solve(r)
   tic
   % Solve the system
   options=odeset('Refine',1,'RelTol',3e-7,'InitialStep',1e-3,'MaxStep',3e6);
-  [t,Y]=ode45(yp,[0 1e7],Y0,options); %1e10
+  [t,Y]=ode45(yp,[0 1e10],Y0,options); %1e10
   toc
   % Compute the instantaneous derivative
   Yp=zeros(length(t)-1,size(Y,2));
