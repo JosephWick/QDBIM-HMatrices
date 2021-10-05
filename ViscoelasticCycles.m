10c3a73f6f84d14e5ebb501fa32df0e3befec7ef
Joseph Wick <josephwick@outlook.com>
2021-10-05 13:58:20 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -34,7 +34,7 @@ function r = build()
   lambdaZ = 35e3; % fault depth
   transition = 40e3; %where shear zone starts
 
-  ss.Ny = 50; %num elems for shear mesh
+  ss.Ny = 51; %num elems for shear mesh
   ss.Nz = 50;
 
   tag = string(probL/1000);
