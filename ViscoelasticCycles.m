3d963b7734c916912ef7fee53650f0cc1adb25a0
Joseph Wick <josephwick@outlook.com>
2021-10-06 18:24:34 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -97,7 +97,7 @@ function r = build()
   ss.polesxr=max(ss.polesxc)+tan((0:edges)'*pi/(2.2*(edges)+eps))*transition;
   ss.polesx=[ss.polesxl(1:end-1);ss.polesxc;ss.polesxr(2:end)];
   % center of shear zone (x2)
-  ss.x2c=shearY_c;
+  ss.x2c=shearY_chat;
 
   % combo mesh
   comboX = [faultX shearX];
