4d330baab5ec433aaf5fcdadf604a74fd3eab030
Joseph Wick <josephwick@outlook.com>
2021-10-28 10:30:54 -0700

    Update test_hmmvp.m

--- test_hmmvp.m
+++ test_hmmvp.m
@@ -49,7 +49,7 @@ function r = build()
 
   x = zeros(1,n);
   y = zeros(1,n);
-  z = [zeros(1,n/2), ones(1,n/2)]s;
+  z = [zeros(1,n/2), ones(1,n/2)];
   c.X = [x; y; z];
 
   kvf('Write', c.kvf, c, 1);
