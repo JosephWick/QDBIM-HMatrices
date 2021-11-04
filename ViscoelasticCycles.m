5aab051b3daf97bc0f1f685055023681a69f022a
Joseph Wick <josephwick@outlook.com>
2021-11-04 10:56:22 -0700

    Update ViscoelasticCycles.m

--- ViscoelasticCycles.m
+++ ViscoelasticCycles.m
@@ -416,7 +416,7 @@ function out = solve(r)
   disp(max(stds(:)))
 
   % make movie
-  Smovie=false;
+  Smovie=true;
   if Smovie
     clf;
     fig = figure;
@@ -442,7 +442,7 @@ function out = solve(r)
   end
 
 
-  Fmovie=false;
+  Fmovie=true;
   if Fmovie
     % velocity movie
     clf;
