e73df43dd9a0d41cfada3ec8504b98a0dc503855
Joseph Wick <josephwick@outlook.com>
2021-11-05 18:30:40 -0700

    Update BP1_visco.m

--- BP1_visco.m
+++ BP1_visco.m
@@ -342,9 +342,14 @@ function y = solve(r)
   Y0=zeros(r.ss.M*r.ss.dgfF+length(r.ss.shearY_chat)*length(r.ss.shearZ_chat)*r.ss.dgfS,1);
 
   % Fault patches
-  Y0(r.ss.M*r.ss.dgfF+1:r.ss.dgfF:2*r.ss.M*r.ss.dgfF)=zeros(size(r.ss.fpTops));
-  Y0(r.ss.M*r.ss.dgfF+2:r.ss.dgfF:2*r.ss.M*r.ss.dgfF)=r.ss.strength;
-  Y0(r.ss.M*r.ss.dgfF+3:r.ss.dgfF:2*r.ss.M*r.ss.dgfF)=log(r.ss.Vo./r.ss.Vpl);
+  % Fault patches
+  Y0(1:ss.dgfF:ss.M*ss.dgfF)=zeros(size(ss.y3f));
+  Y0(2:ss.dgfF:ss.M*ss.dgfF)=ss.strength_W;
+  Y0(3:ss.dgfF:ss.M*ss.dgfF)=log(ss.Vo./ss.Vpl);
+
+  Y0(ss.M*ss.dgfF+1:ss.dgfF:ss.M*ss.dgfF)=zeros(size(ss.y3f));
+  Y0(ss.M*ss.dgfF+2:ss.dgfF:ss.M*ss.dgfF)=ss.strength_E;
+  Y0(ss.M*ss.dgfF+3:ss.dgfF:ss.M*ss.dgfF)=log(ss.Vo./ss.V_plate);
 
   % Shear zones
   Y0(r.ss.M*r.ss.dgfF+1:r.ss.dgfS:end)=r.ss.s120;
