ec22ed08cc8403142b7265b4ac63d63f09931f80
Joseph Wick <josephwick@outlook.com>
2021-11-10 14:16:51 -0800

    Create BP1_visco_d.m

---
+++ BP1_visco_d.m
@@ -0,0 +1,10 @@
+% BP1_visco_m.m
+% dense matrix version of BP1_visco
+
+function varargout = bp1vm (varargin)
+
+  [varargout{1:nargout}] = feval(varargin{:});
+
+end
+
+% ---------------------------- Public ----------------------------
