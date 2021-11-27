cd1d1e57553d3cde02d8ad129db6a0694fddb9c6
Joseph Wick <josephwick@outlook.com>
2021-11-26 17:58:13 -0800

    Update BP1_visco.m

--- BP1_visco.m
+++ BP1_visco.m
@@ -399,8 +399,7 @@ function y = solve(r)
                Yp(:,r.ss.M*r.ss.dgfF+4:r.ss.dgfS:end)'.^2);
 
   % ---       Figures        ---
-  disp(size(Y))
-  disp(size(t))
+  disp(length(t))
   %disp(size(Y(:,3:r.ss.dgfF:r.ss.M*r.ss.dgfF)'))
   y.V = r.ss.Vo.*exp(-Y(:,3:r.ss.dgfF:length(t))'); % Slip rate (m/s)
   y.tau = Y(:,2:r.ss.dgfF:r.ss.M*r.ss.dgfF);            % Shear stress (MPa)
