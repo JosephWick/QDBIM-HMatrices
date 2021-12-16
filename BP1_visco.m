d3b49e52d29d1f6b8719cf4d89261057ac50a074
Joseph Wick <josephwick@outlook.com>
2021-12-16 11:47:20 -0800

    Update BP1_visco.m

--- BP1_visco.m
+++ BP1_visco.m
@@ -379,7 +379,7 @@ function y = solve(r)
   disp('begin solving')
   tic
   options=odeset('Refine',1,'RelTol',1e-8,'InitialStep',1e-5);
-  [t,Y]=ode45(yp,[0 50*3.15e7],Y0,options);
+  [t,Y]=ode45_2(yp,[0 50*3.15e7],Y0,options);
   disp('Done solving');
   toc
 
@@ -398,7 +398,7 @@ function y = solve(r)
   Epall = sqrt( Yp(:,r.ss.M*r.ss.dgfF+3:r.ss.dgfS:end)'.^2 +...
                Yp(:,r.ss.M*r.ss.dgfF+4:r.ss.dgfS:end)'.^2);
 
-  %Y=Y';
+  Y=Y';
   % ---       Figures        ---
   %disp(size(r.ss.Vo
   disp(size(Y))
