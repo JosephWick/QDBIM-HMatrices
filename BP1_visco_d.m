b7dd0622c49f14dd06fc6dbc4d874b1d7b4ae68b
Joseph Wick <josephwick@outlook.com>
2021-12-16 18:10:21 -0800

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -1,12 +1,5 @@
 % BP1_visco_m.m
 % dense matrix version of BP1_visco
-
-function varargout = bp1vm (varargin)
-
-  [varargout{1:nargout}] = feval(varargin{:});
-
-end
-
 % ---     Kernels & functions      ---
 
 G = 30e3;
