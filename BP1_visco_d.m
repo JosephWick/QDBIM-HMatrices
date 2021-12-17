b38f8b26f4542e8683c4a5ebe1b32ea3a3df2aca
Joseph Wick <josephwick@outlook.com>
2021-12-16 19:28:01 -0800

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -196,7 +196,7 @@ for ky=1:length(ss.shearY_chat)
     ss.k1313(:,(kz-1)*ss.Ny+ky) = s1313(shearZ_c(kz)+ss.transition, L(ky), W(kz), ...
       shearY_c'-shearYhat(ky)', shearZ_c');
 
-    ss.k1212f(:,(kz-1)*ss.Nx+ky)=s1212(shearZ_c(kz),L(ky),W(kz),0-shearY_c(ky),ss.fpTops(:)+ss.dz/2);
+    ss.k1212f(:,(kz-1)*ss.Ny+ky)=s1212(shearZ_c(kz),L(ky),W(kz),0-shearY_c(ky),ss.fpTops(:)+ss.dz/2);
     % TODO: add ss.k1312f
 
   end
