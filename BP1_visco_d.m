684caea6885847ae702c3d29471554246d6865ea
Joseph Wick <josephwick@outlook.com>
2021-12-16 18:42:16 -0800

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -174,7 +174,7 @@ ss.k1213f= zeros(length(ss.fpTops), length(shearY_c)*length(faultZ_c));
 ss.k1313f= zeros(length(ss.fpTops), length(shearY_c)*length(faultZ_c));
 
 disp('beginning kernels')
-disp(size(ss.fpTops))
+disp(size(shearY_c(:)))
 % fields from faults
 for k=1:ss.M
   % stress at center of shear zones
