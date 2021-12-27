fe7866e60ba8ec27d1329e94060c83132bf23b9d
Joseph Wick <josephwick@outlook.com>
2021-12-27 13:48:32 -0800

    Update odeBP1v.m

--- odeBP1v.m
+++ odeBP1v.m
@@ -124,12 +124,12 @@ vector = V-ss.Vpl;
 dummy = zeros(ss.Nx*ss.Nz,1);
 X = [vector; dummy];
 t3 = hmmvp('mvp', hm.sf12, X, gM, lM);
-Yp(ss.M*ss.dgfF+1 : ss.dgfS : end) = t1 + t2; %+ t3(ss.M+1:end);
+Yp(ss.M*ss.dgfF+1 : ss.dgfS : end) = t1 + t2 + t3(ss.M+1:end);
 
 t1 = hmmvp('mvp', hm.ss1213, (e12p-ss.e12p_plate));
 t2 = hmmvp('mvp', hm.ss1313, (e13p-ss.e13p_plate));
 t3 = hmmvp('mvp', hm.sf13, X, gM, lM);
-Yp(ss.M*ss.dgfF+2 : ss.dgfS : end) = t1 + t2 + t3(ss.M+1:end);
+Yp(ss.M*ss.dgfF+2 : ss.dgfS : end) = t1 + t2;% + t3(ss.M+1:end);
 
 % Strain rate
 Yp(ss.M*ss.dgfF+3 : ss.dgfS : end) = e12p;
