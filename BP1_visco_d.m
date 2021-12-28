c842389165bd063b5fbc13ca19456fe12c9a5c56
Joseph Wick <josephwick@outlook.com>
2021-12-28 13:21:45 -0800

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -384,7 +384,7 @@ yp=@(t,y) odeBP1v_d(t,y,ss);
 disp('begin solving')
 tic
 options=odeset('Refine',1,'RelTol',1e-8,'InitialStep',1e-5);
-[t,Y]=ode45_2(yp,[0 100*3.15e7],Y0,options);
+[t,Y]=ode45_2(yp,[0 500*3.15e7],Y0,options);
 disp('Done solving');
 toc
 
