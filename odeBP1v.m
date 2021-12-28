10f3bcb8ececb990c258d3959d44e6e249a4e99e
Joseph Wick <josephwick@outlook.com>
2021-12-28 12:03:13 -0800

    Update odeBP1v.m

--- odeBP1v.m
+++ odeBP1v.m
@@ -118,18 +118,18 @@ Yp(4:ss.dgfF:ss.M*ss.dgfF) = (F - ss.b.*ss.sigma.*dth.*f2)./...
 
 % ---       SHEAR         ---
 % Stress rate due to shear zones and fault slip velocity
-t1 = hmmvp('mvp', hm.ss1212, (e12p-ss.e12p_plate));
-t2 = hmmvp('mvp', hm.ss1312, (e13p-ss.e13p_plate));
+%t1 = hmmvp('mvp', hm.ss1212, (e12p-ss.e12p_plate));
+%t2 = hmmvp('mvp', hm.ss1312, (e13p-ss.e13p_plate));
 vector = V-ss.Vpl;
 dummy = zeros(ss.Nx*ss.Nz,1);
 X = [vector; dummy];
-t3 = hmmvp('mvp', hm.sf12, X, gM, lM);
-Yp(ss.M*ss.dgfF+1 : ss.dgfS : end) = t1 + t2;% + t3(ss.M+1:end);
+%t3 = hmmvp('mvp', hm.sf12, X, gM, lM);
+Yp(ss.M*ss.dgfF+1 : ss.dgfS : end) = 0;%t1 + t2;% + t3(ss.M+1:end);
 
-t1 = hmmvp('mvp', hm.ss1213, (e12p-ss.e12p_plate));
-t2 = hmmvp('mvp', hm.ss1313, (e13p-ss.e13p_plate));
-t3 = hmmvp('mvp', hm.sf13, X, gM, lM);
-Yp(ss.M*ss.dgfF+2 : ss.dgfS : end) = t1 + t2 + t3(ss.M+1:end);
+%t1 = hmmvp('mvp', hm.ss1213, (e12p-ss.e12p_plate));
+%t2 = hmmvp('mvp', hm.ss1313, (e13p-ss.e13p_plate));
+%t3 = hmmvp('mvp', hm.sf13, X, gM, lM);
+Yp(ss.M*ss.dgfF+2 : ss.dgfS : end) = 0; %t1 + t2 + t3(ss.M+1:end);
 
 % Strain rate
 Yp(ss.M*ss.dgfF+3 : ss.dgfS : end) = e12p;
