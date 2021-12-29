b0b6af0aab4e3232ab5b49810ca44cb84dcac7cc
Joseph Wick <josephwick@outlook.com>
2021-12-29 14:59:43 -0800

    Update test3.m

--- test3.m
+++ test3.m
@@ -76,7 +76,7 @@ end
 function test_a
   addpaths();
 
-  c.A = [ones(1,10); zeros(1,10); ones(1,10)];
+  c.A = [linspace(1,10,10); linspace(1,10,10); linspace(1,10,10)];
 
   c.greens_fn = 'test';
   c.write_hmat_filename = './tmp/testa';
