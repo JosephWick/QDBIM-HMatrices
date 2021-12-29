854cdf7b1ac297b807a9772a9af9e2aee4416396
Joseph Wick <josephwick@outlook.com>
2021-12-29 15:55:34 -0800

    Update BP1_visco.m

--- BP1_visco.m
+++ BP1_visco.m
@@ -102,7 +102,7 @@ function r = build()
   c.tol = 1e-8;
   c.G = 30e3;
   c.allow_overwrite = 1;
-  c.err_method = 'mrem-abs';
+  c.err_method = 'mrem-fro';
 
   % ---       s12 kernel for fault-fault interaction        ---
   c.greens_fn = 'okadaS12';
