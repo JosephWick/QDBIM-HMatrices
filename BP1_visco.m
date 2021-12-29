cbf082d7e2fba2c9ee538386e7e99663d9919138
Joseph Wick <josephwick@outlook.com>
2021-12-29 15:45:55 -0800

    Update BP1_visco.m

--- BP1_visco.m
+++ BP1_visco.m
@@ -174,6 +174,9 @@ function r = build()
   c.X = [faultX_c; faultY_c; faultZ_c];
   c.Y = [shearX; shearY; shearZ];
 
+  disp(size(c.X))
+  disp(size(c.Y))
+
   kvf('Write', c.kvf, c, 32);
   cmd = ['    ../hmmvp-okada/bin/hmmvpbuild_omp ' c.kvf];
   disp(cmd)
