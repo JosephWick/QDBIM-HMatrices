bd7f00600cda026fb5be652c77ce58d5e6c88420
Joseph Wick <josephwick@outlook.com>
2021-12-28 19:06:25 -0800

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -384,7 +384,7 @@ yp=@(t,y) odeBP1v_d(t,y,ss);
 disp('begin solving')
 tic
 options=odeset('Refine',1,'RelTol',1e-8,'InitialStep',1e-5);
-[t,Y]=ode45_2(yp,[0 1*3.15e1],Y0,options);
+[t,Y]=ode45_2(yp,[0 500*3.15e7],Y0,options);
 disp('Done solving');
 toc
 
@@ -438,7 +438,7 @@ ylabel('Block')
 saveas(gcf, 'figures/BP1vD_strainCenter.png')
 
 % ---         Movies        ---
-Smovie=false;
+Smovie=true;
 if Smovie
   disp('begin shear movie')
   clf;
