ee2a47acc7ccdc7694866b753c622f5733bf517b
Joseph Wick <josephwick@outlook.com>
2022-01-06 13:07:16 -0800

    Update BP1_visco.m

--- BP1_visco.m
+++ BP1_visco.m
@@ -252,7 +252,6 @@ function r = build()
   ss.a=1e-2+Ramp((ss.fpTops-15e3)/3e3)*(0.025-0.01);
   ss.b=0.015*ones(size(ss.fpTops));
 
-  % TODO: is the sigma the same as sigmab?
   % effective normal stress (MPa)
   ss.sigma=50.0*ones(size(ss.fpTops));
 
@@ -267,13 +266,6 @@ function r = build()
 
   % Radiation damping coefficient
   ss.eta = G./(2*Vs);
-
-  % static friction coefficient
-  ss.mu0 = 0.2*ones(size(ss.fpTops));
-
-  % fault strength
-  ss.strength = ss.sigma.*(ss.mu0+(ss.a-ss.b).*log(ss.Vpl./ss.Vo))+...
-    G*ss.Vpl./(2*Vs);
 
   % Estimates of some key parameters
   VWp = find(ss.a < ss.b); % VW region
