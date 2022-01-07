4ade20579f048f7f9ebb23bc7fcac965604161ac
Joseph Wick <josephwick@outlook.com>
2022-01-07 10:28:41 -0800

    Update odeBP1v.m

--- odeBP1v.m
+++ odeBP1v.m
@@ -105,8 +105,8 @@ v = e12p-ss.e12p_plate;
 %disp(hmmvp('getm', hm.fs1212))
 
 t1 = hmmvp('mvp', hm.s12, V-ss.Vpl);
-%t2 = hmmvp('mvp', hm.fs1212, v);
-t3 = hmmvp('mvp', hm.fs1312, v);
+t2 = hmmvp('mvp', hm.fs1212, v);
+%t3 = hmmvp('mvp', hm.fs1312, v);
 
 F = t1 + t2;% + t3;
 
