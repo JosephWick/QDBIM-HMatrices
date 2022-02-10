affee7aa52b4aedce2305160e4d46bf7514c363b
Joseph Wick <josephwick@outlook.com>
2022-02-09 16:31:29 -0800

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -232,7 +232,7 @@ function out = run()
       ss.fs1312(:,(kz-1)*ss.Ny+ky)=s1312(shearZhat(kz), L(ky), W(kz), ...
         0-shearYhat(ky)', ss.fpTops+ss.dz/2);
 
-        if (ky == 51) & (kz == 1)
+        if (ky == 3) & (kz == 3)
           disp( (kz-1)*ss.Ny+ky )
           disp(L(ky))
           disp(W(kz))
