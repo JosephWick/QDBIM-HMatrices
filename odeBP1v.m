44cd9dba9b8b6f922f33fdf5d9031ef03e78f023
Joseph Wick <josephwick@outlook.com>
2022-02-19 21:46:05 -0800

    Update odeBP1v.m

--- odeBP1v.m
+++ odeBP1v.m
@@ -107,7 +107,7 @@ t1 = hmmvp('mvp', hm.s12, V-ss.Vpl);
 %t2 = hmmvp('mvp', hm.fs1212, e12p - ss.e12p_plate);
 %t3 = hmmvp('mvp', hm.fs1312, e13p - ss.e13p_plate);
 
-F = t1 + t2 + t3;
+F = t1;% + t2 + t3;
 %F = t1 + t3;
 
 f1 = 2*ss.Vo./V.*exp(-(ss.fo+ss.b.*th)./ss.a);
