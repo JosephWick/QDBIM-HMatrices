933639c3197195eeffa3d77657ebaf21dca6ecae
Joseph Wick <josephwick@outlook.com>
2022-02-20 10:45:09 -0800

    Update odeBP1v_d.m

--- odeBP1v_d.m
+++ odeBP1v_d.m
@@ -93,9 +93,9 @@ e13p = tau13 .* Aeff;
 % Shear stress rate on fault due to fault and shear zones
 t1 = ss.ff12 * (V-ss.Vpl);
 %disp(size(e12p-ss.e12p_plate))
-%t2 = ss.fs1212 * (e12p-ss.e12p_plate);
+t2 = ss.fs1212 * (e12p-ss.e12p_plate);
 %t3 = ss.fs1312 * (e13p-ss.e13p_plate);
-F = t1;% + t2 + t3;
+F = t1 + t2;% + t3;
 
 f1 = 2*ss.Vo./V.*exp(-(ss.fo+ss.b.*th)./ss.a);
 f2 = 1./sqrt(1+f1.^2);
