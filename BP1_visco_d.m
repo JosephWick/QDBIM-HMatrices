9fbacf73dadcd19959ce262d2f8082ab192743d1
Joseph Wick <josephwick@outlook.com>
2022-03-09 11:42:26 -0800

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -487,7 +487,7 @@ function out = run()
   saveas(gcf, 'figures/BP1vD_strainCenter.png')
 
   % ---         Movies        ---
-  Smovie=true;
+  Smovie=false;
   if Smovie
     disp('begin shear movie')
     clf;
@@ -513,7 +513,7 @@ function out = run()
   disp('shear movie done')
   end
 
-  Fmovie=true;
+  Fmovie=false;
   if Fmovie
     % fault movie
     disp('begin fault movie')
