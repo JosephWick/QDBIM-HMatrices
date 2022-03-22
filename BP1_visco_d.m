cd908613f833472a5dc7b78bd6cf03b3a68069d3
Joseph Wick <josephwick@outlook.com>
2022-03-21 17:31:52 -0700

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -455,7 +455,7 @@ function out = run()
   Epall = sqrt( Yp(:,ss.M*ss.dgfF+3:ss.dgfS:end)'.^2 +...
                Yp(:,ss.M*ss.dgfF+4:ss.dgfS:end)'.^2);
 
-  Epall = Y(:,ss.M*ss.dgfF+1:ss.dgfS:end)' - ss.s120;
+  %Epall = Y(:,ss.M*ss.dgfF+1:ss.dgfS:end)' - ss.s120;
 
   % ---       Figures        ---
   %disp(size(r.ss.Vo
