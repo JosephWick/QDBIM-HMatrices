75abd4c2eef329ed27c8c3e32369fc5738548265
Joseph Wick <josephwick@outlook.com>
2022-03-30 16:43:24 -0500

    Update TwoFaults.m

--- TwoFaults.m
+++ TwoFaults.m
@@ -470,7 +470,7 @@ function out = run()
   tic
   % Solve the system
   options=odeset('Refine',1,'RelTol',3e-7,'InitialStep',1e-3,'MaxStep',3e6);
-  [t,Y]=ode45(yp,[0 100*3.15e7],Y0,options);
+  [t,Y]=ode45(yp,[0 200*3.15e7],Y0,options);
   disp('done')
   toc
   %%
