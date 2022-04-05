0d0389d3484ab219c266f2fbd2f32a6e5ae07d61
Joseph Wick <josephwick@outlook.com>
2022-04-05 14:24:22 -0700

    Update BP1_visco_d.m

--- BP1_visco_d.m
+++ BP1_visco_d.m
@@ -227,7 +227,7 @@ function out = run()
 
   % adapt naming convention
   shearZhat = ss.polesz;
-  shearY_c = xx2c(:)';
+  shearY_c = xx2c(:);
   shearZ_c = xx3c(:)';
   ss.shearY_chat = ss.x2c';
   ss.shearZ_chat = ss.x3c';
@@ -274,7 +274,7 @@ function out = run()
     ss.ff12(:,k)=s12h(0, ss.fpTops+ss.dz/2, y2_W, ss.fpTops(k), Wf(k));
 
     % stress at center of shear zones
-    ss.sf12(:,k)=s12h(shearY_c', shearZ_c', y2_W, ss.fpTops(k), Wf(k));
+    ss.sf12(:,k)=s12h(shearY_c, shearZ_c', y2_W, ss.fpTops(k), Wf(k));
     ss.sf13(:,k)=s13h(shearY_c', shearZ_c', y2_W, ss.fpTops(k), Wf(k));
 
   end
