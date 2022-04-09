ef486f95a1f0583e4a5857b8aec4267e516a778d
Joseph Wick <josephwick@outlook.com>
2022-04-09 15:32:27 -0700

    Update BP1_visco_hm2.m

--- BP1_visco_hm2.m
+++ BP1_visco_hm2.m
@@ -196,9 +196,6 @@ function r = build()
   c.X = [faultX_c; faultY_c; faultZ_c];
   c.Y = [shearXhat'; ss.shearY_chat'; shearZhat'];
   c.Z = [shearX(:)'; shearY(:)'; shearZ(:)']; % hm sizing purposes only
-  disp(size(c.X))
-  disp(size(c.Y))
-  disp(size(c.Z))
 
   c.L = L;
   c.W = W;
@@ -229,6 +226,9 @@ function r = build()
   c.Y = [faultX; faultY; faultZ'];
   c.L = Lf;
   c.W = Wf;
+
+  disp(size(c.X))
+  disp(size(c.Y))
 
   c.greens_fn = 'okadaS12';
   c.write_hmat_filename = './tmp/BP1v_sf-s12';
