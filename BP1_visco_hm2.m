878333807c45a53d4763de7882a6ee3c142e8032
Joseph Wick <josephwick@outlook.com>
2022-04-09 15:24:28 -0700

    Update BP1_visco_hm2.m

--- BP1_visco_hm2.m
+++ BP1_visco_hm2.m
@@ -149,7 +149,7 @@ function r = build()
 
   c.transition = ss.transition;
   c.Y = [shearXhat; ss.shearY_chat; shearZhat];
-  c.X = [shearX_c(:); shearY_c(:); shearZ_c(:)];
+  c.X = [shearX_c(:); shearY_c(:); shearZ_c(:)]; c.X = c.X';
 
   c.Z = [shearX; shearY; shearZ]; % for sizing purposes only
 
