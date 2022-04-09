215a80317e49594bdca51825a742566732ad45c9
Joseph Wick <josephwick@outlook.com>
2022-04-09 16:07:36 -0700

    Update BP1_visco_hm2.m

--- BP1_visco_hm2.m
+++ BP1_visco_hm2.m
@@ -432,7 +432,7 @@ function out = run(b)
   tic
   % Solve the system
   options=odeset('Refine',1,'RelTol',3e-7,'InitialStep',1e-3,'MaxStep',3e6);
-  [t,Y]=ode45_2(yp,[0 1*3.15e7],Y0,options);
+  [t,Y]=ode45_2(yp,[0 500*3.15e7],Y0,options);
   disp('done solving.')
   toc
   %%
@@ -506,6 +506,8 @@ function out = run(b)
     end
   disp('shear movie done')
   end
+
+  out = 1;
 
 end
 
