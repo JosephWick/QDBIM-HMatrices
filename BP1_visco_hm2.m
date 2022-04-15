0b4a7f97ef3980bff057323897864e9ab0148a79
Joseph Wick <josephwick@outlook.com>
2022-04-14 18:48:48 -0700

    Update BP1_visco_hm2.m

--- BP1_visco_hm2.m
+++ BP1_visco_hm2.m
@@ -456,26 +456,26 @@ function out = run(b)
   % % % % % % % % % % % % % % % % % % % % % % % % % % % %%
 
   % save entire Y vector
-  tmp = mat2np(Y, 'pickles/BP1vHM2_Y.pkl', 'float64');
+  mat2np(Y, 'pickles/BP1vHM2_Y.pkl', 'float64');
 
   % Strain rate at center
   Ep=sqrt(Yp(:,ss.M*ss.dgfF+floor(length(ss.x2c)/2)*ss.dgfS+3:ss.dgfS* ...
             length(ss.x2c):end)'.^2 +...
           Yp(:,ss.M*ss.dgfF+floor(length(ss.x2c)/2)*ss.dgfS+4:ss.dgfS* ...
             length(ss.x2c):end)'.^2);
-  tmp = mat2np(Ep, 'pickles/BP1vHM2_strainCenter.pkl', 'float64');
+  mat2np(Ep, 'pickles/BP1vHM2_strainCenter.pkl', 'float64');
 
 
   % strain rate over whole ductile area
   Epall = sqrt( Yp(:,ss.M*ss.dgfF+3:ss.dgfS:end)'.^2 +...
                Yp(:,ss.M*ss.dgfF+4:ss.dgfS:end)'.^2);
-  tmp = mat2np(Epall, 'pickles/BP1vHM2_strainAll.pkl', 'float64');
+  mat2np(Epall, 'pickles/BP1vHM2_strainAll.pkl', 'float64');
 
   % Velocity
   y.V = Yp(:,1:ss.dgfF:ss.M*ss.dgfF); % Slip rate (m/s)
   y.V = y.V';
   y.tau = Y(:,2:ss.dgfF:ss.M*ss.dgfF);            % Shear stress (MPa)
-  tmp = mat2np(y.V, 'pickles/BP1vHM2_V.pkl', 'float64');
+  mat2np(y.V, 'pickles/BP1vHM2_V.pkl', 'float64');
 
   % fault slip figure
   clf;
